ae27d87e5899b9a86c258b9482e718b4fd116adc
Safat11 <safat3622@gmail.com>
2024-12-27 19:10:19 +0600

    Login Form Field Validation Using Regular Expression

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,22 +5,46 @@ import app from '../../firebase/firebase.config';
 const auth = getAuth(app);
 
 const Register = () => {
-
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('')
     const handleSubmit = (event) =>{
-
+        // 1. prevent page refress
         event.preventDefault();
+
+        setSuccess('');
+        setError('');
+        // 2. collect from data
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
 
-        // create user in firebase
+        // validate
+        if(!/(?=.*[A-Z])/.test(password)){
+            setError("Please add at least one upper case");
+            return;
+        }
+        else if(!/(?=.*[0-9].*[0-9])/.test(password)){
+            setError('Please add at least two number')
+            return;
+        }
+        else if(password.length<6){
+            setError('Please add at least 6 characters in your password')
+            return;
+        }
+
+        // 3. create user in firebase
         createUserWithEmailAndPassword(auth, email, password)
         .then(result => {
             const loggedUser = result.user;
             console.log(loggedUser);
+            setError('');
+            event.target.reset();
+            setSuccess('User has been created successfully');
         })
         .catch(error => {
-            console.error(error);
+            console.error(error.message);
+            setError(error.message);
+           
         })
 
     }
@@ -28,7 +52,7 @@ const Register = () => {
     const [email, setEmail] = useState('');
     const handleEmailChange = (event) =>{
         // console.log(event.target.value);
-        setEmail(event.target.value);
+        // setEmail(event.target.value);
     }
 
     const handlePasswordBlur = (event) =>{
@@ -39,12 +63,14 @@ const Register = () => {
             <h4>Please Register</h4>
 
             <form onSubmit={handleSubmit}>
-            <input className='w-50 mb-4 rounded ps-2' onChange={handleEmailChange} type="email" name="name" id="email" placeholder='Your E-mail' />
+            <input className='w-50 mb-4 rounded ps-2' onChange={handleEmailChange} type="email" name="name" id="email" placeholder='Your E-mail' required />
             <br />
-            <input className='w-50 mb-4 rounded ps-2' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' />
+            <input className='w-50 mb-4 rounded ps-2' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' required />
             <br />
             <input className='btn btn-primary' type="submit" value="Register" />
             </form>
+            <p className='text-danger'>{error}</p>
+            <p className='text-primary'>{success}</p>
         </div>
     );
 };
